e6d1807692ff735b6cb2797dd8c18f66320fc3dc
Dario Blancá <darioblanca.lp@gmail.com>
2018-12-23 00:05:44 -0300

    Agregue segundo cliente

--- cliente.py
+++ cliente.py
@@ -7,3 +7,9 @@ connection = server.connect(("localhost", 9999))
 time.sleep(1)
 connection.send("Manteca de cacao")
 connection.close()
+
+try:
+	cliente2 = server.connect(("localhost", 9998))
+	cliente2.send("Me quiero volver chango")
+except:
+	print "Hubo un problema con el segundo cliente"
