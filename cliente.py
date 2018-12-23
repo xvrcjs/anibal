6517862cc9577d89c90428a3db9a742d0c845196
Dario Blancá <darioblanca.lp@gmail.com>
2018-12-23 00:08:14 -0300

    Agregue mas mensajes

--- cliente.py
+++ cliente.py
@@ -6,4 +6,8 @@ server = socket.socket()
 connection = server.connect(("localhost", 9999))
 time.sleep(1)
 connection.send("Manteca de cacao")
+time.sleep(1)                                                         │
+connection.send("Uingardum lebiosa")
+time.sleep(1)                                                         │
+connection.send("Lumus")
 connection.close()
