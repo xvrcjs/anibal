6c97829a04cb577a38e206f42ba10f73fb87336d
Dario Blancá <darioblanca.lp@gmail.com>
2018-12-22 23:44:52 -0300

    Cambio de puerto a 9998

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 server = socket.socket()
-server.bind(("localhost", 9999))
+server.bind(("localhost", 9998))
 server.listen(5)
 cliente, datos = server.accept()
 mensaje = cliente.recv(1024)
